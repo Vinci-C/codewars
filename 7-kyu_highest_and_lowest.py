d9ee304251c3cea81b9454cc5458c0ef888cc023
Macrohard Onfire <36289506+pikca-c@users.noreply.github.com>
2024-02-29 21:56:11 +0000

    Create 7-kyu_highest_and_lowest.py

---
+++ 7-kyu_highest_and_lowest.py
@@ -0,0 +1,5 @@
+def high_and_low(numbers):
+    numbers = list(map(int, numbers.split()))
+    # ...
+    return "{} {}".format(max(numbers),min(numbers))
+  #first time using mapping in a codewars training
