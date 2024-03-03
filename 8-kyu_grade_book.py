a69a422a071f81265032fe25e0374607d7a61782
Macrohard Onfire <36289506+pikca-c@users.noreply.github.com>
2024-03-03 21:21:16 +0000

    Create 8-kyu_grade_book.py

---
+++ 8-kyu_grade_book.py
@@ -0,0 +1,13 @@
+#very easy excerise
+def get_grade(s1, s2, s3):
+	mean = (s1 + s2 + s3) / 3
+	if mean >= 90:
+		return "A"
+	elif mean >= 80:
+		return "B"
+	elif mean >= 70:
+		return "C"
+	elif mean >= 60:
+		return "D"
+	else:
+		return "F"
